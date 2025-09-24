8a94b4c2118505d4d18e0bf04c89e6c75054a561
iosonobert <andrew.zulberti@gmail.com>
2025-09-24 12:52:01 +0800

    Setonix SLURM

---
+++ extract_exif.py
@@ -0,0 +1,39 @@
+imgdir = './SMODE_IOP2_MASS_DOPPVIS_20230427_005616_055-20230427_010241_055'
+
+import glob, os
+import rasterio, pandas as pd
+from tqdm import tqdm
+
+import matplotlib.pyplot as plt
+
+files = glob.glob(imgdir + '/*.tif')
+
+## Change this
+import exiftool
+EXIF_TOOL_full = '/usr/local/bin/exiftool'
+EXIF_TOOL_full = r'/home/andrew/Image-ExifTool-12.69/exiftool'
+a
+imgsearch = '*/*.tif'
+
+# outzarr  = os.path.join(field_dir, f'processed_data/{camera}/{camera}_V3_DASK.zarr')
+def parse_exif(fname):
+    with exiftool.ExifTool(EXIF_TOOL_full) as et:
+        metadata = et.execute_json(fname)
+        df_meta  = pd.DataFrame(metadata)
+
+    return df_meta
+
+tiff_files = glob.glob(f"{imgdir}{imgsearch}", recursive=True)
+print(len(tiff_files), ' found.\nConverting...')
+
+print('Reading EXIF data...')
+df = []
+for ff in tqdm(tiff_files):
+    df.append(parse_exif(ff))
+    
+
+exif = pd.concat(df)
+ds_exif = exif.to_xarray().rename({'index':'time'})
+
+print(ds_exif)
+print('Done.')
