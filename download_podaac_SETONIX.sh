22b79f4465a9975739bef0726686411b22e4924b
Andrew Zulberti <azulberti@setonix-02.head.cm.setonix.pawsey.org.au>
2025-10-30 16:41:41 +0800

    Made dowload PODAAC flexible for LWIR

--- download_podaac_SETONIX.sh
+++ download_podaac_SETONIX.sh
@@ -1,15 +1,24 @@
-# Activate your virtual environment
+: Activate your virtual environment
 source $MYSOFTWARE/smode_env/bin/activate
 
-# Make sure download folder exists
-mkdir -p $MYSCRATCH/DOPPVIS
+echo "Downloading PODAAC SETONIX data"
 
 # Usage: ./download_script.sh 2023-04-19
 DATE="$1"
 STARTTIME="$2"
 ENDTIME="$3"
+INSTRUMENT="$4"
+
+if [ -z "$INSTRUMENT" ]; then
+    INSTRUMENT=LWIR
+fi
+echo "Instrument: $INSTRUMENT"
+
+# Make sure download folder exists
+mkdir -p $MYSCRATCH/$INSTRUMENT
 
 if [ -z "$DATE" ]; then
+    echo ERROR: Date argument is required.
     echo "Usage: $0 YYYY-MM-DD"
     exit 1
 fi
@@ -20,12 +29,12 @@ if [ -z "$ENDTIME" ]; then
     ENDTIME=23:59:59
 fi
 
-echo "Downloading data for date: $DATE from $STARTTIME to $ENDTIME"
+echo "Downloading $INSTRUMENT data for date: $DATE from $STARTTIME to $ENDTIME"
 
 # Run the download
 podaac-data-downloader \
-  -c SMODE_L1_MASS_DOPPVIS_V1 \
-  -d $MYSCRATCH/DOPPVIS \
+  -c SMODE_L1_MASS_${INSTRUMENT}_V1 \
+  -d $MYSCRATCH/$INSTRUMENT \
   --start-date ${DATE}T${STARTTIME}Z \
   --end-date ${DATE}T${ENDTIME}Z \
   -e ""
